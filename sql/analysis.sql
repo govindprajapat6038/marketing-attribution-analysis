b171761206ffada30b6a04f61bfb3884c3118daf
Govind Prajapat <govindprajapatsrdr@gmail.com>
2026-06-22 21:13:17 +0530

    Added Avg... revenue

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -22,10 +22,35 @@ SELECT Channel,
        SUM(Revenue)/SUM(Cost) AS ROAS
 FROM marketing_data
 GROUP BY Channel;
-
+-- total revenue
 SELECT Campaign_ID,
        SUM(Revenue) AS Total_Revenue
 FROM marketing_data
 GROUP BY Campaign_ID
 ORDER BY Total_Revenue DESC
 LIMIT 5;
+--c1 conversion rate
+SELECT Channel,
+       ROUND(SUM(Conversions)/SUM(Clicks)*100,2) AS Conversion_Rate
+FROM marketing_data
+GROUP BY Channel
+ORDER BY Conversion_Rate DESC;
+-- c1 cost per conversion
+SELECT Channel,
+       ROUND(SUM(Cost)/SUM(Conversions),2) AS Cost_Per_Conversion
+FROM marketing_data
+GROUP BY Channel
+ORDER BY Cost_Per_Conversion;
+
+--c1 profit
+SELECT Channel,
+       SUM(Revenue)-SUM(Cost) AS Profit
+FROM marketing_data
+GROUP BY Channel
+ORDER BY Profit DESC;
+--c1 Avg.. revenue
+SELECT Channel,
+       ROUND(AVG(Revenue),2) AS Avg_Revenue
+FROM marketing_data
+GROUP BY Channel
+ORDER BY Avg_Revenue DESC;
